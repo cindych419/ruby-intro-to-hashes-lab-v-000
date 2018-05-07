3aa7c676b519773a6210b8f683e97de83bf916f7
cindych419@gmail.com <cindych419@gmail.com>
2018-05-07 21:38:21 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:name => " "}
+	monopoly = {:  => " "}
 
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
