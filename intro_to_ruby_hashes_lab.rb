234d0339cc25e78ebc227df3431c11f00a44045b
cindych419@gmail.com <cindych419@gmail.com>
2018-05-07 21:46:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,6 @@ end
 
 def actor
 	actor = {:name  => "Dwayne The Rock Johnson"}
-	actor[:name]
 
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
